050bb476099677a391199cc6db13da3f988916b7
BigBrosh <maksuhin@gmail.com>
2018-05-30 15:19:07 +0300

    - added aside in chat; - added chat styles

--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../styles/chat_page/chat.sass';
+
 import RequestController from '../controllers/RequestController';
 
 import createBrowserHistory from 'history/createBrowserHistory';
@@ -8,6 +10,7 @@ const history = createBrowserHistory();
 class mainPage extends React.Component {
 	componentWillMount = () => {
 		let logged = RequestController.getFromLocal('logged');
+
 		if (logged === false ||
 			!logged)
 		{
@@ -18,7 +21,92 @@ class mainPage extends React.Component {
 
 	render = () => {		
 		return (
-			<h1>Chat will be there</h1>
+			<div class="main_chat_wrap">
+				<div class="aside">
+					<div class="top_line">
+						<button>Create chat</button>
+					</div>
+
+					<ul class="available_chats">
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Very big last message from somebody</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+
+						<li class="chat">
+							<p class="chat_title">Nickname</p>
+							<p class="chat_preview">Last message</p>
+						</li>
+					</ul>
+				</div>
+
+				<div class="chat_wrap"></div>
+			</div>
 		);
 	}
 }
