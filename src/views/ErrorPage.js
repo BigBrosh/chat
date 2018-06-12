a93ec938d5771d5c0875ef8790efe6c67c629084
BigBrosh <maksuhin@gmail.com>
2018-06-12 15:50:39 +0300

    styles modified

--- src/views/ErrorPage.js
+++ src/views/ErrorPage.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 export const ErrorPage = () => 
-	<div>
-		<p style={{marginBottom: 10}}>404<br/>Page not found</p>
+	<div style={{
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			textAlign: 'center',
+			paddingTop: 30
+		}}>
+
+		<p style={{marginBottom: 10, fontSize: 20}}><span style={{fontSize: 32}}>404</span><br/>
+		Page not found</p>
 		<Link to='/'>Open home page</Link>
 	</div>
